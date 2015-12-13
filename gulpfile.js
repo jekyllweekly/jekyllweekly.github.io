18073accbee92f27cc4caf578c8325efda272707
bradonomics <brad@bradonomics.com>
2015-12-13 15:24:28 +0700

    Removed unused plugins and functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,6 @@
 var gulp        = require('gulp'),
     shell       = require('gulp-shell'),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    rename      = require('gulp-rename'),
-    newer       = require('gulp-newer'),
-    imagemin    = require('gulp-imagemin'),
-    // watch       = require('gulp-watch');
     browserSync = require('browser-sync');
-
-
-//* Scripts
-gulp.task('vendorjs', function() {
-  return gulp.src('./_js/vendor/*.js')
-    .pipe(concat('vendor.js'))
-    .pipe(uglify())
-    .pipe(rename({
-      basename: "vendor",
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./js/'));
-});
-
-gulp.task('themejs', function() {
-  return gulp.src('./_js/*.js')
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(rename({
-      basename: "main",
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./js'));
-});
-
-
-//* Images
-gulp.task('images', function () {
-    return gulp.src('./_images/*.{png,jpg,gif}')
-        // .pipe(watch('./_images/**/*'))
-        // .pipe(newer('./_images/*.{png,jpg,gif}'))
-        .pipe(imagemin())
-        .pipe(gulp.dest('./images/'));
-});
 
 
 //* BrowserSync
@@ -50,14 +10,12 @@ gulp.task('browser-sync', function () {
       baseDir: '_site/'
     }
   });
-  // gulp.watch('_js/*.js', ['themejs']);
   gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
 
 //* Run Jekyll build and serve commands
 gulp.task('build', shell.task(['jekyll build --config _config-dev.yml --watch']));
-// gulp.task('serve', shell.task(['jekyll serve']));
 
 
 // Default task (build and serve)
